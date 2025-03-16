b47f00ec331c0b3cc3ef79d01e5b5add14cb6147
Torstenkliesch <torsten.kliesch@gmx.de>
2025-03-16 23:06:59 +0100

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,1 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar-gef')
-    sidebar.style.display = 'flex'
-  } 
-
-  function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar-gef')
-    sidebar.style.display = 'none'
-  }  
+ 
